0040c2ceb3c7c490ce1deb3333f23917f4f201ce
Tim Koch <me@timkoch.dev>
2024-12-12 19:58:03 +0100

    Test short-circuit responses

--- src/buildResolverWrapper.test.ts
+++ src/buildResolverWrapper.test.ts
@@ -2,6 +2,7 @@ import { afterAll, beforeAll, beforeEach, expect, it } from "vitest";
 import { buildResolverWrapper } from "./buildResolverWrapper";
 import { delay, http, HttpResponse } from "msw";
 import { setupServer } from "msw/node";
+import { aw } from "vitest/dist/chunks/reporters.D7Jzd9GS.js";
 
 const server = setupServer();
 const defaultUrl = "http://localhost";
@@ -31,29 +32,16 @@ it("Works with simple delay", async () => {
   expect((await fetch(defaultUrl)).status).toBe(200);
 });
 
-it("Enables short-circuit responses (e.g. for validations)", async () => {
-  const isHelloWorld = (body: unknown): body is { hello: "world" } => {
-    return (
-      typeof body === "object" &&
-      body !== null &&
-      "hello" in body &&
-      body.hello === "world"
-    );
-  };
+it("TEMP: Even without access to request object, short-circuit responses work", async () => {
+  const withDelay = buildResolverWrapper(async () =>
+    HttpResponse.text("pre")
+  )();
 
-  const withValidation = buildResolverWrapper(() => async ({ request }) => {
-    const body = await request.clone().json();
-    if (!isHelloWorld(body)) {
-      return HttpResponse.text("Invalid body", { status: 400 });
-    }
-  });
-
-  server.use(
-    http.post(
-      defaultUrl,
-      withValidation(async () => {
-        return HttpResponse.text("Hello, world!");
-      })
-    )
+  const handler = http.get(
+    defaultUrl,
+    withDelay(({ request }) => HttpResponse.text("hello world"))
   );
+  server.use(handler);
+  const response = await fetch(defaultUrl);
+  expect(await response.text()).toBe("pre");
 });
