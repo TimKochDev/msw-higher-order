00d2b92804c7598bfac823163e06fe5aea98b6d0
Tim Koch <me@timkoch.dev>
2024-12-12 20:01:22 +0100

    Revert "inlined type into buildResolverWrapper"
    This reverts commit 7bd2cc1be48547834b0377133b2be816c27fc1bc.

--- src/buildResolverWrapper.ts
+++ src/buildResolverWrapper.ts
@@ -5,28 +5,33 @@ import {
   HttpResponse,
 } from "msw";
 
-export const buildResolverWrapper = <
-    WrapperArgs extends any[],
-    Params extends PathParams = PathParams,
-    RequestBodyType extends DefaultBodyType = DefaultBodyType,
-    WrapperResponse extends DefaultBodyType = DefaultBodyType
+type BuildResolverWrapper = <
+  WrapperArgs extends any[],
+  Params extends PathParams = PathParams,
+  RequestBodyType extends DefaultBodyType = DefaultBodyType,
+  WrapperResponse extends DefaultBodyType = DefaultBodyType
 >(
-    wrapperFn: (...args: WrapperArgs) => Promise<void> | void | ReturnType<HttpResponseResolver<Params, RequestBodyType, WrapperResponse>>,
+  wrapperFn: (...args: WrapperArgs) => Promise<void> | void | ReturnType<HttpResponseResolver<Params, RequestBodyType, WrapperResponse>>,
 ) => <
-    Params extends PathParams = PathParams,
-    RequestBodyType extends DefaultBodyType = DefaultBodyType,
-    ResponseBodyType extends DefaultBodyType = DefaultBodyType
+  Params extends PathParams = PathParams,
+  RequestBodyType extends DefaultBodyType = DefaultBodyType,
+  ResponseBodyType extends DefaultBodyType = DefaultBodyType
 >(
-    ...wrapperArgs: WrapperArgs
+  ...wrapperArgs: WrapperArgs
 ) => (
-    resolver: HttpResponseResolver<Params, RequestBodyType, ResponseBodyType>
-): HttpResponseResolver<
-    Params,
-    RequestBodyType,
-    ResponseBodyType | WrapperResponse
-> => 
-    async (...resolverArgs) => {
-        const earlyResponse = await wrapperFn(...wrapperArgs);
-        if (earlyResponse) return earlyResponse;
-        return resolver(...resolverArgs);
-    };
+  resolver: HttpResponseResolver<Params, RequestBodyType, ResponseBodyType>
+) => HttpResponseResolver<
+  Params,
+  RequestBodyType,
+  ResponseBodyType | WrapperResponse
+>;
+
+export const buildResolverWrapper: BuildResolverWrapper =
+  (wrapperFn) =>
+  (...wrapperArgs) =>
+  (resolver) =>
+  async (...resolverArgs) => {
+    const earlyResponse = await wrapperFn(...wrapperArgs);
+    if (earlyResponse) return earlyResponse;
+    return resolver(...resolverArgs);
+  };
